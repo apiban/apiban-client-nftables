fa1a3c0c0db82e22746a6ac27e8faffa4bba369a
Fred Posner <fred@pgpx.io>
2025-01-16 12:39:44 -0500

    license change: MIT

--- main.go
+++ main.go
@@ -1,25 +1,34 @@
-// apiban-client-nftables - add apiban.org data to a nftables set
-//
-// Copyright (C) 2025 Fred Posner
-// Copyright (C) 2025 The Palner Group, Inc.
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-//
-// Example build commands:
-// GOOS=linux GOARCH=amd64 go build -o apiban-client-nftables
-// GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o apiban-client-nftables
-// GOOS=linux GOARCH=arm GOARM=7 go build -o apiban-client-nftables-pi
+/*
+apiban-client-nftables - add apiban.org data to a nftables set
+
+The MIT License (MIT)
+
+Copyright (c) 2025 Fred Posner
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Example build commands:
+GOOS=linux GOARCH=amd64 go build -o apiban-client-nftables
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o apiban-client-nftables
+GOOS=linux GOARCH=arm GOARM=7 go build -o apiban-client-nftables-pi
+
+*/
 
 package main
 
